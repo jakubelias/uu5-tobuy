1c45e1a8dea757fb7ce024931c51fab61700c6b5
Filip Veleminsky <12-470-1@plus4u.net>
2017-06-13 14:18:22 +0200

    static resources fix

--- uu_appg01_template-server/src/main/java/uu/app/controllers/HomeController.java
+++ uu_appg01_template-server/src/main/java/uu/app/controllers/HomeController.java
@@ -1,31 +1,40 @@
 package uu.app.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import uu.app.services.HelloService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping("/{tid}-{awid}")
 public class HomeController {
 
-  @Autowired
-  private HelloService service;
-
   @RequestMapping(method = RequestMethod.GET)
   public void home(HttpServletRequest request, HttpServletResponse httpServletResponse) throws IOException {
-    httpServletResponse.sendRedirect(request.getRequestURI().toString() + "/public");
+    httpServletResponse.sendRedirect(request.getRequestURI().toString() + "/home");
   }
 
   @RequestMapping(path = "/*", method = RequestMethod.GET)
-  public String about(@PathVariable String tid, @PathVariable String awid) {
-    return "forward:/"+tid+"-"+awid+"/public/index.html";
+  public String about() {
+    return "forward:/index.html";
+  }
+
+  @RequestMapping(path = "/public/**", method = GET)
+  public String resource(HttpServletRequest request) throws IOException {
+    Pattern p = Pattern.compile(".*/public/(.*)");
+    Matcher matcher = p.matcher(request.getRequestURI());
+    if (matcher.matches()) {
+      String resourcePath = matcher.group(1);
+      return "forward:/"+resourcePath;
+    }
+    return "forward:/index.html";
   }
 
 }
