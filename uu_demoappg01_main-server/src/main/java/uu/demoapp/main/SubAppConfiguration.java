dd2bf27d925d1314b2ea534df9f66bcf889e98d1
Petr Ciochon <petr.ciochon@plus4u.net>
2017-08-10 12:54:55 +0200

    disable authentication

--- uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppConfiguration.java
+++ uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppConfiguration.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import uu.app.authentication.oidc.OpenIdConnectContextConfiguration;
 import uu.app.authentication.oidc.handler.OpenIdConnectHandlerConfiguration;
 import uu.app.datastore.annotations.DataStoreConfiguration;
@@ -21,7 +24,7 @@ import uu.app.validation.ValidationTypeDefinitionSource;
  * Spring configuration of the application.
  */
 @DataStoreConfiguration
-@Import({ValidationContextConfiguration.class, UseCaseServerContextConfiguration.class, DatastoreMongoDbContextConfiguration.class, OpenIdConnectHandlerConfiguration.class})
+@Import({ValidationContextConfiguration.class, UseCaseServerContextConfiguration.class, DatastoreMongoDbContextConfiguration.class, OpenIdConnectHandlerConfiguration.class,WebSecurityConfig.class})
 public class SubAppConfiguration extends AbstractMongoDbContextConfiguration {
 
   @Value("${objectStoreUri}")
