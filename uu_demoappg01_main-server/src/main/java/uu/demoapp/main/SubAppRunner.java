88b0d3f5d56518451e93cb4ca7f5e802d92b8c79
Filip Veleminsky <12-470-1@plus4u.net>
2017-08-10 16:30:56 +0200

    Fixed application deployment to C3. Using new version of gradle plugin to allow deploy configuration. Gitignore fixes.

--- uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppRunner.java
+++ uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppRunner.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -12,7 +14,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @Import(value = {SubAppConfiguration.class})
-public class SubAppRunner {
+public class SubAppRunner extends SpringBootServletInitializer {
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(SubAppRunner.class, args);
