c134cc4734f173f99c45498ffac076cefa977c5b
Jiri Neuman <jiri.neuman@unicornsystems.eu>
2017-08-14 15:59:48 +0200

    UAFJ-142 Application template extended to use the cover server project

--- uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppRunner.java
+++ uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppRunner.java
@@ -1,20 +1,12 @@
 package uu.demoapp.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Import;
+import uu.app.subapp.AbstractSubAppRunner;
 
 /**
  * Class for running the application.
  */
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
-@Import(value = {SubAppConfiguration.class})
-public class SubAppRunner extends SpringBootServletInitializer {
+public class SubAppRunner extends AbstractSubAppRunner {
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(SubAppRunner.class, args);
