32fea4ce5946f41bc33e6ccaa57b3d353b2beed5
Filip Veleminsky <12-470-1@plus4u.net>
2017-08-14 16:44:06 +0200

    Changed location of static content to *-server/public and configured routing for local development and production.

--- uu_demoappg01_main-client/config/config.js
+++ uu_demoappg01_main-client/config/config.js
@@ -25,8 +25,7 @@ module.exports.getConfig = function () {
 
     // build settings
     sourcePath: srcDir,
-    // outputPath: (isWithoutServer ? "public" : `../${serverDirName}/public`), // file system folder to build files to
-    outputPath: ("public"), // file system folder to build files to
+    outputPath: (isWithoutServer ? "public" : `../${serverDirName}/public`), // file system folder to build files to
     entryPoints: buildHelpers.getHtmlFilesFromMappingsJson()
                     .map(html => html.replace(/\.html?$/i, ".js"))
                     .filter(jsFile => fs.existsSync(path.join(srcDir, jsFile))), // files to build (relative to "src/", resp. sourcePath)
