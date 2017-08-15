799568fa2ac1fec30d2a8148315678fb62054a58
Jiri Neuman <jiri.neuman@unicornsystems.eu>
2017-08-15 13:05:22 +0200

    UAFJ-142 Application template extended to use the cover server project - test fix

--- uu_demoappg01_main-server/src/test/java/uu/demoapp/main/main/test/DemoApplicationTests.java
+++ uu_demoappg01_main-server/src/test/java/uu/demoapp/main/main/test/DemoApplicationTests.java
@@ -1,29 +1,39 @@
 package uu.demoapp.main.main.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Collections;
 import javax.inject.Inject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import uu.app.authorization.Authorization;
+import uu.app.authorization.DefaultAuthorizationContext;
+import uu.demoapp.main.SubAppRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = SubAppRunner.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 
   protected static final String CONTEXT_PATH = "/uu-demoappg01-main";
   @Inject
   protected WebApplicationContext wac;
   protected MockMvc mockMvc;
+
+  @MockBean
+  protected Authorization authorization;
 
   /**
    * Test preparation.
@@ -34,6 +44,7 @@ public class DemoApplicationTests {
         .webAppContextSetup(this.wac)
         .defaultRequest(get(CONTEXT_PATH).contextPath(CONTEXT_PATH))
         .build();
+    when(authorization.authorize(any(), any())).thenReturn(new DefaultAuthorizationContext(true, Collections.emptySet()));
   }
 
   @Test
