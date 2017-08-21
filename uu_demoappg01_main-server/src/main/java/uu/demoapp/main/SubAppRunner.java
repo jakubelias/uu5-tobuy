6d70694f9eae4b6e4e0fdd33a6921e8d43f4a538
Jiri Neuman <jiri.neuman@unicornsystems.eu>
2017-08-21 16:32:09 +0200

    UAFJ-199 Default profiles configuration changed

--- uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppRunner.java
+++ uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppRunner.java
@@ -1,6 +1,9 @@
 package uu.demoapp.main;
 
+import static org.springframework.core.env.AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME;
+
 import org.springframework.boot.SpringApplication;
+import uu.app.core.core.UuProfiles;
 import uu.app.subapp.AbstractSubAppRunner;
 import uu.app.subapp.annotation.UuSubApp;
 
@@ -10,7 +13,15 @@ import uu.app.subapp.annotation.UuSubApp;
 @UuSubApp
 public class SubAppRunner extends AbstractSubAppRunner {
 
+  /**
+   * The main method starts the application and is intended only for a non-production use.
+   *
+   * @param args application arguments
+   * @throws Exception any uncaught exception
+   */
   public static void main(String[] args) throws Exception {
+    // The main method is not expected to be used in production, but only for development -> set the default profile to development
+    System.setProperty(DEFAULT_PROFILES_PROPERTY_NAME, UuProfiles.DEVELOPMENT);
     SpringApplication.run(SubAppRunner.class, args);
   }
 
