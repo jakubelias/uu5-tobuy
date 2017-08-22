39ad5af985892807917e0d950e3df5580a2683cb
Jiří Dudek <jdk@Jiri-MacBook-Pro-2.local>
2017-08-22 17:25:03 +0200

    Fix wrong component scan in server-server.

--- uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppRunner.java
+++ uu_demoappg01_main-server/src/main/java/uu/demoapp/main/SubAppRunner.java
@@ -2,10 +2,12 @@ package uu.demoapp.main;
 
 import org.springframework.boot.SpringApplication;
 import uu.app.subapp.AbstractSubAppRunner;
+import uu.app.subapp.annotation.UuSubApp;
 
 /**
  * Class for running the application.
  */
+@UuSubApp
 public class SubAppRunner extends AbstractSubAppRunner {
 
   public static void main(String[] args) throws Exception {
